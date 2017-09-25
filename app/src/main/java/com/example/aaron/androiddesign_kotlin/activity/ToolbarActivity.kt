ad64933c3c08f06ebf82987d211db2c3725cae97
chenkezhao <310771881@qq.com>
2017-09-25 23:51:39 +0800

    Toolbar update CollapsingToolbarLayout 内容可以与应用栏重叠:应用程序栏有两个滚动选项 主要布局：CoordinatorLayout>AppBarLayout>CollapsingToolbarLayout>Toolbar

--- app/src/main/java/com/example/aaron/androiddesign_kotlin/activity/ToolbarActivity.kt
+++ app/src/main/java/com/example/aaron/androiddesign_kotlin/activity/ToolbarActivity.kt
@@ -1,8 +1,9 @@
 package com.example.aaron.androiddesign_kotlin.activity
 
 import android.os.Bundle
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.aaron.androiddesign_kotlin.R
 import com.example.aaron.androiddesign_kotlin.adapter.BaseDataRVAdapter
@@ -15,16 +16,26 @@ class ToolbarActivity : BaseActivity() {
         setContentView(R.layout.activity_toolbar)
         rvList.apply {
             //1.添加线条
-            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+//            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
             //2.设置布局管理器
-            layoutManager = LinearLayoutManager(context).apply {
-                orientation = LinearLayoutManager.VERTICAL
-            }
+//            layoutManager = LinearLayoutManager(context).apply {
+//                orientation = LinearLayoutManager.VERTICAL
+//            }
+            //线性宫格显示，瀑布流
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
             //3.adapter
-            adapter = BaseDataRVAdapter(initData()){
-                Toast.makeText(context,"点击了"+(it), Toast.LENGTH_LONG).show()
+            adapter = BaseDataRVAdapter(initData(),BaseDataRVAdapter.TYPE_TOOLBAR_ACTIVITY) {
+                Toast.makeText(context, "点击了" + (it), Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar_activity,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return true
     }
 
     private fun initData(): Array<String> {
--- app/src/main/java/com/example/aaron/androiddesign_kotlin/adapter/BaseDataRVAdapter.kt
+++ app/src/main/java/com/example/aaron/androiddesign_kotlin/adapter/BaseDataRVAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.aaron.androiddesign_kotlin.adapter
 
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -11,14 +12,30 @@ import kotlinx.android.synthetic.main.item_basedata.view.*
  *注：参数var menus: Array<String>，则val itemClick: (String)
  * Created by Administrator on 2017/9/23.
  */
-class BaseDataRVAdapter(var datas: Array<String>, val itemClick: (String) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BaseDataRVAdapter(var datas: Array<String>,val type:Int = 1, val itemClick: (String) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        val TYPE_TOOLBAR_ACTIVITY = 2
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return type
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        return ViewHolder1(LayoutInflater.from(parent?.context).inflate(R.layout.item_basedata, parent, false) as TextView, itemClick)
+        if(viewType == TYPE_TOOLBAR_ACTIVITY){
+            return ViewHolder2(LayoutInflater.from(parent?.context).inflate(R.layout.item_toolbar, parent, false) as CardView, itemClick)
+        }else{
+            return ViewHolder1(LayoutInflater.from(parent?.context).inflate(R.layout.item_basedata, parent, false) as TextView, itemClick)
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        (holder as ViewHolder1).bindData(datas[position])
+        if(holder is ViewHolder1){
+            holder.bindData(datas[position])
+        }else if(holder is ViewHolder2){
+            holder.bindData(datas[position],position)
+        }
     }
 
     override fun getItemCount(): Int = datas.size
@@ -27,6 +44,16 @@ class BaseDataRVAdapter(var datas: Array<String>, val itemClick: (String) -> Uni
         fun bindData(text: String) {
             itemView.tvText.text = text
             itemView.setOnClickListener { itemClick(text) }
+        }
+    }
+    class ViewHolder2(itemView: CardView?, val itemClick: (String) -> Unit) : RecyclerView.ViewHolder(itemView) {
+        fun bindData(text: String,position:Int) {
+            itemView.apply {
+                tvText.text = text
+                //手动更改高度，不同位置的高度有所不同
+                tvText.height = (500 + (position % 3) * 100)
+                setOnClickListener { itemClick(text) }
+            }
         }
     }
 
